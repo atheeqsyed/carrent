7667b18a71a7211b400f4bb1666cca292189ecff
atheeqsyed <atheeqmvj@gmail.com>
2022-08-15 19:14:27 +0530

    files modofied

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@ function Connect()
     $dbpass = "A@#345_abcd1";
     $dbname = "carrental";
 
-    //Create Connection
+    //Create DB Connection
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
 
     return $conn;
