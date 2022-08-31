b0430701827ce8d83c973cf0f907dfe12b0458f7
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-08-31 08:33:13 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ function Connect()
     $dbpass = "A@#345_abcd1";
     $dbname = "carrental";
     
-    //Create DB Conn 
+    //Create Database connection
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
     return $conn;
 }
